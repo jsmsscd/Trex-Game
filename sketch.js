7923129dbf4f33d76419e7e98c31be9392cfa51d
jsmsscd <66997718+jsmsscd@users.noreply.github.com>
2020-11-27 12:31:29 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -68,7 +68,7 @@ function setup() {
   invisibleGround.visible = false;
   
   trex.setCollider("rectangle",0,0,400,50);
-  trex.debug=true;
+ // trex.debug=true;
   
   cloudsGroup = new Group();
   obstaclesGroup = new Group();
